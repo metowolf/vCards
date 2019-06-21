cf729fd302b536c26a8ed85278aacd0cc31b40a6
metowolf <i@i-meto.com>
2019-06-21 20:10:49 +0800

    🌋️ 新增 vcf 合并版本

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,45 +1,51 @@
+const del = require('del')
 const path = require('path')
 const globby = require('globby')
 const through2 = require('through2')
 
-const gulp = require('gulp')
+const { series, src, dest } = require('gulp')
 const ava = require('gulp-ava')
 const zip = require('gulp-zip')
 const concat = require('gulp-concat')
 const rename = require("gulp-rename")
+const concatFolders = require('gulp-concat-folders')
 
 const plugin_vcard = require('./plugins/vcard')
 
-gulp.task('generator', () => {
-  return gulp.src('data/*/*.yaml')
+const generator = () => {
+  return src('data/*/*.yaml')
     .pipe(through2.obj(plugin_vcard))
     .pipe(rename({ extname: '.vcf' }))
-    .pipe(gulp.dest('./temp'))
-})
+    .pipe(dest('./temp'))
+}
 
-gulp.task('archive', () => {
-  return gulp.src(['temp/*/*.vcf', 'temp/*.all.vcf'])
+const archive = () => {
+  return src('temp/**')
     .pipe(zip('archive.zip'))
-    .pipe(gulp.dest('./public'))
-})
+    .pipe(dest('./public'))
+}
 
-gulp.task('concat', async () => {
-  const paths = await globby('temp/*', { onlyFiles: false })
-  const types = paths.map(x => x.split('/')[1])
-  const promises = types.map(type => {
-    return gulp.src(`temp/${type}/*.vcf`)
-      .pipe(concat(`${type}.all.vcf`))
-      .pipe(gulp.dest('./temp'))
-  })
-  await Promise.all(promises)
-  return gulp.src('temp/*.all.vcf')
-    .pipe(concat(`all.vcf`))
-    .pipe(gulp.dest('./public'))
-})
+const combine = () => {
+  return src('temp/*/*.vcf')
+    .pipe(concatFolders('temp'))
+    .pipe(rename({ extname: '.all.vcf' }))
+    .pipe(dest('./'))
+}
 
-gulp.task('test', () => {
-  return gulp.src('src/test.js')
+const test = () => {
+  return src('./src/test.js')
     .pipe(ava({verbose: true}))
-})
+}
 
-gulp.task('build', gulp.series('test', 'generator', 'concat', 'archive'))
+const clean = () => {
+  return del([
+    'public',
+    'temp'
+  ])
+}
+
+exports.test = test
+exports.generator = generator
+exports.combine = combine
+exports.archive = archive
+exports.build = series(test, clean, generator, combine, archive)
